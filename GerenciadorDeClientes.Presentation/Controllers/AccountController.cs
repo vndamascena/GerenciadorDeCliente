97c6870ceabc1fcd75d1de539413051174380119
vndamascena@gmail.com <vndamascena@gmail.com>
2023-07-19 19:45:06 -0300

    feat: Desenvolvimento Front-End das paginas Login, Register e ForgotPassword

---
+++ GerenciadorDeClientes.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorDeClientes.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
